ab8a1c24026ec54bbfb63fbed1c093eac46eef75
Nguyen Thai <nguyenthai@MacBook-Pro-cua-Nguyen.local>
2017-03-16 10:15:28 +0700

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen, SQLite } from 'ionic-native';
+
+import { HomePage } from '../pages/home/home';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage = HomePage;
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+      let db = new SQLite();
+            db.openDatabase({
+                name: "data.db",
+                location: "default"
+            }).then(() => {
+                db.executeSql("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)", {}).then((data) => {
+                    console.log("TABLE CREATED: ", data);
+                    alert ("create table people sussecfull")
+                }, (error) => {
+                    console.error("Unable to execute sql", error);
+                })
+            }, (error) => {
+                console.error("Unable to open database", error);
+            });
+    });
+  }
+}
